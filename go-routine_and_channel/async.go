af15955c873f165a251de96f29e96fe07c639cf5
woowonjin <dndnjswls613@naver.com>
2022-04-18 13:41:01 +0900

    Async와 channel 다시 공부

---
+++ go-routine_and_channel/async.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func say(s string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(s, "***", i)
+	}
+}
+
+func main() {
+	say("sync")
+
+	go say("Async1")
+	go say("Async2")
+	go say("Async3")
+
+	time.Sleep(time.Second * 3)
+}
+
+
